36229f6569bfa0a5def95e5da73a99dc095e12dd
Devin <devinmui@yahoo.com>
2015-07-14 13:21:04 -0700

    idk

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+for(var all=document.getElementsByTagName("*"),i=0,max=all.length;max>i;i++)all[i].removeAttribute("style");links=document.styleSheets;for(var i=0,max=links.length;max>i;i++)links[i].disabled=!0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	\
