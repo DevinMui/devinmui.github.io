05d475d29e0c4af65a3f4e0b6b28a85fc3351a08
Devin Mui <devinwmui@gmail.com>
2022-06-28 13:00:06 -1000

    monke

--- src/pages/home/components/hero/hero.tsx
+++ src/pages/home/components/hero/hero.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { ReactComponent as HeroImg } from './hero.svg'
+import React from "react";
+import { ReactComponent as HeroImg } from "./hero.svg";
 
 function Hero() {
-    return (
-        <section className="hero">
-            <HeroImg className="hero-img" />
-            <div className="hero-text">
-                <h1>Software Engineer</h1>
-                <p>Let's Build Cool Things Together</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="hero">
+      <HeroImg className="hero-img" />
+      <div className="hero-text">
+        <h1>Code Monkey</h1>
+        <p>Let's Build Cool Things Together</p>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
